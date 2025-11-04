1a6caf193330fdf63ec12e1429700a53d32c2de5
ahmedaliqureshi <ahmedaliqureshi59@gmail.com>
2025-11-04 12:38:14 +0000

    cal area of triangle

---
+++ CalculateArea.js
@@ -0,0 +1,37 @@
+// there are maily 2 mehtods to calculate 
+// the area of the triangle
+
+// 1st method:- is use of the base and hegiht and then deviding it by half 
+// logic= (base*height)/ 1/2
+console.log('using 1st method:- \n');
+console.log('we are going to declare the varibale and there value \n');
+
+// we are going to declare the variable as let of base and height 
+
+let base= 4; 
+let height= 10; 
+
+// now we are going to make an varibale and assign the formula to it 
+
+console.log('then we ar going to use the formula of (base*height)/1/2 \n');
+let formula= (base*height)/ 1/2; 
+console.log('then we are going to print the vlaue= \n');
+console.log(formula);
+
+
+// the 2nd method is the use of the sides to multiply 
+
+console.log('2nd method:-');
+console.log('then we are going to use the formula of side*side*side\n');
+let side1=3; 
+let side2=3; 
+let side3=3; 
+console.log('then we are going to multiply 3 sides of the triangle \n');
+let formula2= side1*side2*side3; 
+console.log('the result is= \n');
+console.log(formula2);
+
+
+
+
+
